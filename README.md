41d15e6f8de0088361ff10ea77a5d5d087aea165
Ask sky <2096358571@qq.com>
2023-06-08 18:26:35 +0800

    ci: fix bilibili.yml

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # iel-notebook
 
 基于Vitepress构建的笔记本
+
+## 更新情况
+
+---start---
+
+---end---
--- pyapp/getVideo.py
+++ pyapp/getVideo.py
@@ -1,4 +1,9 @@
+import datetime
 import json
+import os
+import re
+import time
+import pytz
 import requests
 import config
 
@@ -44,8 +49,22 @@ def write_json(json_data: list) -> None:
         f.write(content)
 
 
+def update_readme() -> None:
+    file_url = '/home/runner/work/iel-notebook/iel-notebook/./README.md'
+    insert_info = "---start---\n\n##" + "数据更新时间:" + datetime.fromtimestamp(int(time.time()), pytz.timezone(
+        'Asia/Shanghai')).strftime(
+        '%Y-%m-%d %H:%M:%S') + "\n\n---end---"
+    # 获取README.md内容
+    with open(os.path.join(os.getcwd(), file_url), 'r', encoding='utf-8') as f:
+        readme_md_content = f.read()
+    new_readme_md_content = re.sub(r'---start---(.|\n)*---end---', insert_info, readme_md_content)
+    with open(os.path.join(os.getcwd(), file_url), 'w', encoding='utf-8') as f:
+        f.write(new_readme_md_content)
+
+
 if __name__ == '__main__':
     data1 = get_pubdate('10', 'Cookie☆ NYN', 'cookie')
     data2 = get_pubdate('10', '俊达萌', '俊达萌')
     w_data = data1 + data2
     write_json(w_data)
+    update_readme()
